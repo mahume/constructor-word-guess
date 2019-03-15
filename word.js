4972749d13614e6ad6e1614c14f18f35f62ae405
mahume <michael.a.hume@gmail.com>
2019-03-14 18:00:17 -0600

    update

--- word.js
+++ word.js
@@ -12,21 +12,19 @@ const Word = function(word) {
             console.log(this.newLetterArr[i].checkGuess(guess))
         }
     }
+    this.displayWord = function() {
+        // call function on each letter object
+        let str = ''
+        for (let i = 0; i < this.newLetterArr.length; i++) {
+            str += this.newLetterArr[i].returnCharacter()
+        }
+        console.log(str)
+        // func.characterReturn
+        // concatenate together
+        // return string representation of word
+    }
 }
 
 const newWord = new Word('Mike')
 newWord.checkLetter('i')
-
-/*
-this.displayWord = function() {
-    // call function on each letter object
-    let str = ''
-    for (let i = 0; i < this.newLetterArr.length; i++) {
-        str += this.newLetterArr[i].letter
-    }
-    console.log(str)
-    // func.characterReturn
-    // concatenate together
-    // return string representation of word
-}
-*/
+newWord.displayWord()
