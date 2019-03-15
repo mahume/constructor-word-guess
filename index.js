55c9b5bdca1ffdf239b7bb6a8f70dd4e7dee18e6
mahume <michael.a.hume@gmail.com>
2019-03-15 10:06:28 -0600

    Done for the day

--- index.js
+++ index.js
@@ -2,32 +2,39 @@ const Word = require('./word')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 
-const algo = new Word('Foo Bar')
-algo.checkLetter('o')
-algo.displayWord()
 
-let remainingGuesses;
+let remainingGuesses = 10
 const wordsComputerScience = ['Algorithm', 'Data Structures', 'Framework', 'Object Oriented Programming']
 const wordsForeignLanguages = ['Polish', 'Hungarian', 'Mandarin', 'Icelandic']
 const wordsMovies = ['The Green Mile', 'Good Will Hunting', 'Shawshank Redemption', 'V for Vendetta']
 
-
-function randomizeWord(arr) {
-    let randomNum = Math.floor(Math.random() * arr.length) + 1
-    let randomWord = words[randomNum]
-    let newWord = new Word
-}
-
-/*
 inquirer
 .prompt([
     {
         type: 'list',
         name: 'topic',
-        message: 'Pick a hangman topic.',
+        message: `Welcome to Word Guess! Select a topic to get started.`,
         choices: ['Computer Science', 'Foreign Languages', 'Movies']
     }
 ]).then(function(answers) {
-    
+    switch (answers.topic) {
+        case 'Computer Science':
+            randomizeWord(wordsComputerScience)
+            break;
+        case 'Foreign Languages':
+            randomizeWord(wordsForeignLanguages)
+            break;
+        case 'Movies':
+            randomizeWord(wordsMovies)
+            break; 
+        default:
+            break;
+    }
 })
-*/
+
+function randomizeWord(arr) {
+    let randomNum = Math.floor(Math.random() * arr.length)
+    let randomWord = arr[randomNum]
+    let newWord = new Word(randomWord)
+    newWord.displayWord()
+}
