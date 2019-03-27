a20cbc7a75b65a0673ca68246c76aa73f5a11fc6
mahume <michael.a.hume@gmail.com>
2019-03-27 10:41:19 -0600

    update

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const chalk = require('chalk')
 let remainingGuesses = 9
 let wordToGuess, newWord
 let lettersGuessed = []
-const wordsComputerScience = ['Algorithm', 'Data Structures', 'Framework', 'Object Oriented Programming']
-const wordsForeignLanguages = ['Polish', 'Hungarian', 'Mandarin', 'Icelandic']
-const wordsMovies = ['The Green Mile', 'Good Will Hunting', 'Shawshank Redemption', 'V for Vendetta']    
+const WORDS_COMPUTER_SCIENCE = ['Algorithm', 'Data Structures', 'Framework', 'Boolean', 'Closure', 'Array', 'Iteration']
+const WORDS_FOREIGN_LANGUAGES = ['Polish', 'Hungarian', 'Mandarin', 'Icelandic', 'Hebrew', 'Arabic']
+const WORDS_MOVIES = ['The Green Mile', 'Good Will Hunting', 'Shawshank Redemption', 'V for Vendetta', 'Casablanca', 'The Godfather']    
 
 selectTopicPrompt()
 function selectTopicPrompt() {
@@ -22,35 +22,45 @@ function selectTopicPrompt() {
             choices: ['Computer Science', 'Foreign Languages', 'Movies']
         }
     ]).then(answers => {
-        switch (answers.topic) {
-            case 'Computer Science':
-                pickRandomWord(wordsComputerScience)
-                break;
-            case 'Foreign Languages':
-                pickRandomWord(wordsForeignLanguages)
-                break;
-            case 'Movies':
-                pickRandomWord(wordsMovies)
-                break; 
-            default:
-                break;
-        }
+        let topic = answers.topic
+        chooseTopicArray(topic)
     })
 }
+function chooseTopicArray(topic) {
+    switch (topic) {
+        case 'Computer Science':
+            pickRandomWord(WORDS_COMPUTER_SCIENCE)
+            break;
+        case 'Foreign Languages':
+            pickRandomWord(WORDS_FOREIGN_LANGUAGES)
+            break;
+        case 'Movies':
+            pickRandomWord(WORDS_MOVIES)
+            break; 
+        default:
+            break;
+    }
+}
 function pickRandomWord(arr) {
-    const randomNum = Math.floor(Math.random() * arr.length)
-    const randomWord = arr[randomNum]
+    let randomNum = Math.floor(Math.random() * arr.length)
+    let randomWord = arr[randomNum]
     newWord = new Word(randomWord)
     wordToGuess = newWord.wordToArr.map(letter => letter.toLowerCase())
     removeSpaces()
     guessSetup(newWord)
 }
+function removeSpaces() {
+    for (let i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i] === ' ') {
+            wordToGuess.splice([i], 1)
+        }
+    }
+}
 function guessSetup(newWord) {
     console.log('')
     newWord.displayWord()
-    console.log(wordToGuess)
     if (checkIfWon() || checkIfLost()) {
-        playAgain()
+        playAgainPrompt()
         return
     } 
 
@@ -65,42 +75,38 @@ function guessLetterPrompt(newWord) {
             message: 'Guess a letter!'
         }
     ]).then(answers => {
-        const guess = answers.guess.toLowerCase()
-        alreadyGuessed(guess)
+        let guess = answers.guess.toLowerCase()
+        alreadyGuessed(guess, newWord)
         newWord.checkLetter(guess)
-
-        if (wordToGuess.includes(guess)) {
-            for (let i = 0; i < wordToGuess.length; i++) {
-                if (guess === wordToGuess[i]) {
-                    wordToGuess.splice([i], 1);
-                }
-            }
-            lettersGuessed.push(guess)
-            console.log(chalk.underline(`Correct!`))
-            console.log('')
-            guessSetup(newWord)
-        } else {
-            console.log(chalk.underline(`Sorry! Wrong letter. Keep guessing.`))
-            console.log(chalk.italic(`Guesses Remaining: ${chalk.red(remainingGuesses)}`))
-            console.log('')
-            remainingGuesses--
-            guessSetup(newWord)
-        }
+        afterGuess(guess, newWord)
     })
 }
-
-function removeSpaces() {
-    for (let i = 0; i < wordToGuess.length; i++) {
-        if (wordToGuess[i] === ' ') {
-            wordToGuess.splice([i], 1)
-        }
-    }
-}
-function alreadyGuessed(letter) {
+function alreadyGuessed(letter, newWord) {
     if (lettersGuessed.includes(letter)) {
         console.log(`You've already guessed that. Try another letter`)
-        guessLetterPrompt()
+        guessSetup(newWord)
     }
+}
+function afterGuess(letter, newWord) {
+    if (wordToGuess.includes(letter)) {
+        for (let i = 0; i < wordToGuess.length; i++) {
+            if (wordToGuess[i] === letter) {
+                wordToGuess.splice([i], 1)
+            }
+        }
+        lettersGuessed.push(letter)
+        console.log(chalk.underline(`Correct!`))
+        console.log('')
+        guessSetup(newWord)
+    } else {
+        console.log(chalk.underline(`Sorry! Wrong letter. Keep guessing.`))
+        console.log(chalk.italic(`Guesses Remaining: ${chalk.red(remainingGuesses)}`))
+        console.log('')
+        remainingGuesses--
+        guessSetup(newWord)
+    }
+
+
 }
 function checkIfWon() {
     if (wordToGuess.length === 0) {
@@ -114,7 +120,7 @@ function checkIfLost() {
         return true
     }
 }
-function playAgain() {
+function playAgainPrompt() {
     inquirer
     .prompt([
         {
@@ -127,7 +133,7 @@ function playAgain() {
             remainingGuesses = 9
             selectTopicPrompt()
         } else {
-            console.log('Goodbye')
+            console.log('Goodbye for now!')
             process.exit()
         }
     })
